6572ab485e35bf0fd911c2dbe8fd350287004cfb
Vinicius Souza <viniciusls22@gmail.com>
2019-03-18 21:32:00 -0300

    Inicio relatorio 2
    Signed-off-by: Vinicius Souza <viniciusls22@gmail.com>

--- COM1/relatorio2/exercicio1.m
+++ COM1/relatorio2/exercicio1.m
@@ -4,7 +4,7 @@ clc;
 
 Ac = 1; % Amplitude portadora
 Am = 1; % Amplitude sinal
-fc = 50e3; % Frequencia da portadora
+fc = 20e3; % Frequencia da portadora
 fm = 1e3; % Frequencia do sinal
 fa = 20*fc; % Frequencia de amostragem
 f_cut = 4e3; % Frequencia de corte filtro fir1
@@ -29,48 +29,81 @@ info_dsb_sc = filter(filtro_PB, 1, r_t_dsb_sc);
 figure(1)
 subplot(511)
 plot(t, m_t)
-legend('m(t)')
+title('Sinal de Informação - m(t)=sin(2*pi*1000*t)')
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
 subplot(512)
 plot(t, c_t)
-legend('c(t)')
+title('Portadora - c(t)=cos(2*pi*50000*t)')
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
 subplot(513)
 plot(t, s_t_dsb_sc)
-legend('s(t)')
+title('Sinal Modulado - s(t)')
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
 subplot(514)
 plot(t, r_t_dsb_sc)
-legend('r(t)')
+title('Sinal Demodulado - r(t)')
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
 subplot(515)
 plot(t, info_dsb_sc);
-legend('m(t)')
+title('Sinal Demodulado e filtrado (informação)')
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
 
 
 % • Para o caso da modulaçao AM DSB, variar o 'fator de
 % modulação' (0.25; 0.5; 0.75 e 1 e 1.5) e observar os efeitos no
 % sinal modulado
 figure(2)
-m = 0.25; Ao = Am/m;
+m = 0.25; Ao = Am/m
 s_t_dsb = (m_t + Ao).*c_t; % sinal modulado em dsb
 subplot(511)
 plot(t, s_t_dsb)
-legend('m = 0.25')
-m = 0.5; Ao = Am/m;
+title(['Sinal Modulado - m=0.25 A0=', num2str(Ao)])
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
+m = 0.5; Ao = Am/m
 s_t_dsb = (m_t + Ao).*c_t;
 subplot(512)
 plot(t, s_t_dsb)
-legend('m = 0.5')
-m = 0.75; Ao = Am/m;
+title(['Sinal Modulado - m=0.5 A0=', num2str(Ao)])
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
+m = 0.75; Ao = Am/m
 s_t_dsb = (m_t + Ao).*c_t;
 subplot(513)
 plot(t, s_t_dsb)
-legend('m = 0.75')
-m = 1; Ao = Am/m;
+title(['Sinal Modulado - m=0.75 A0=', num2str(Ao)])
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
+m = 1; Ao = Am/m
 s_t_dsb = (m_t + Ao).*c_t;
 subplot(514)
 plot(t, s_t_dsb)
-legend('m = 1')
-m = 1.5; Ao = Am/m;
+title(['Sinal Modulado - m=1 A0=', num2str(Ao)])
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
+
+m = 1.5; Ao = Am/m
 s_t_dsb = (m_t + Ao).*c_t;
 subplot(515)
 plot(t, s_t_dsb)
-legend('m = 1.5')
+title(['Sinal Modulado - m=1.5 A0=', num2str(Ao)])
+xlabel('Tempo [s]')
+ylabel('Amplitude [V]')
 
+% figure(3)
+% subplot(211)
+% plot(t, m_t)
+% subplot(212)
+% plot(t, c_t)
